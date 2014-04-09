26a1b47fee12f6a4a0b950c369096c4b11da18fe
Tim Snadden <tim@snadden.com>
2014-04-09 16:29:11 +1200

    whitespace

--- code/CustomHtmlEditorConfig.php
+++ code/CustomHtmlEditorConfig.php
@@ -1,50 +1,49 @@
 <?php
 /**
- *	This basically just exists to give us getter methods for protected properties of HTMLEditorConfig
- *	We "wrap" the HTMLEditorConfig object in an HTMLEditorConfig subclass so it has access to the protected $settings property
- *	This actually just stores the identifier rather than the object itself because we need to know the identifier for the JS.
- *	
- *	It also provides a copy() static method that duplicates editor configs for convenience
- *	
+ * This basically just exists to give us getter methods for protected properties of HTMLEditorConfig
+ * We "wrap" the HTMLEditorConfig object in an HTMLEditorConfig subclass so it has access to the protected $settings property
+ * This actually just stores the identifier rather than the object itself because we need to know the identifier for the JS.
+ *
+ * It also provides a copy() static method that duplicates editor configs for convenience
+ *
  */
 class CustomHTMLEditorConfig extends HTMLEditorConfig {
-	
+
 	/**
-	 *	The identifier string
+	 * The identifier string
 	 */
 	public $configIdentifier;
-	
-	
+
+
 	/**
-	 *	Constructor is passed the identifier of teh HTMLEditorConfig we want to wrap
-	 *	
-	 *	@param string $identifier	 Can't use an instance of HTMLEditorConfig to create because we need to know the identifier
+	 * Constructor is passed the identifier of the HTMLEditorConfig we want to wrap
+	 *
+	 * @param string  $identifier Can't use an instance of HTMLEditorConfig to create because we need to know the identifier
 	 */
 	function __construct($identifier) {
 		$this->configIdentifier = $identifier;
 	}
-	
+
 	/**
-	 *	Used like HTMLEditorConfig::get() but returns the config wrapped in an CustomHTMLEditorConfig
-	 *	
-	 *	@param string $identifier
+	 * Used like HTMLEditorConfig::get() but returns the config wrapped in an CustomHTMLEditorConfig
+	 *
+	 * @param string  $identifier
 	 */
 	static function get($identifier = 'default') {
 		return new CustomHTMLEditorConfig($identifier);
 	}
-	
-	
+
+
 	/**
-	 *	Creates a new config by cloning an existing one.  Just a helplful utility function
-	 *	
-	 *	@param string $newIdentifier	The ID of the new config we're creating
-	 *	@param mixed HtmlEditorConfig|string $old	The old config we're copying, either as an ID string or the object itself
-	 *	@param string $name Option friendly name for the new config.  If left blank, friendly_name is set to the new ID
-	 *	
-	 *	@return HTMLEditorConfig
+	 * Creates a new config by cloning an existing one.  Just a helplful utility function
+	 *
+	 * @param string  $newIdentifier The ID of the new config we're creating
+	 * @param mixed   HtmlEditorConfig|string $old The old config we're copying, either as an ID string or the object itself
+	 * @param string  $name          Option friendly name for the new config.  If left blank, friendly_name is set to the new ID
+	 *
+	 * @return HTMLEditorConfig
 	 */
 	static function copy($newIdentifier, $old, $name = null) {
-	
 		if (is_string($old)) {
 			$old = HTMLEditorConfig::get($old);
 		}
@@ -59,21 +58,19 @@ class CustomHTMLEditorConfig extends HTMLEditorConfig {
 
 		$newConfig->buttons = $old->buttons;
 
-		
 		if ($name) {
 			$newConfig->setOption('friendly_name', $name);
 		} else {
 			$newConfig->setOption('friendly_name', $newIdentifier);
 		}
-		
+
 		return $newConfig;
 	}
-	
-	
+
 	/**
-	 *	Returns the settings array of the wrapped config
-	 *	
-	 *	@return array
+	 * Returns the settings array of the wrapped config
+	 *
+	 * @return array
 	 */
 	function getSettings() {
 		return $this->getConfig()->settings;
@@ -81,9 +78,9 @@ class CustomHTMLEditorConfig extends HTMLEditorConfig {
 
 
 	/**
-	 *	Returns the wrapped config
-	 *	
-	 *	@return HtmlEditorConfig
+	 * Returns the wrapped config
+	 *
+	 * @return HtmlEditorConfig
 	 */
 	function getConfig() {
 		return HTMLEditorConfig::get($this->configIdentifier);
@@ -92,9 +89,9 @@ class CustomHTMLEditorConfig extends HTMLEditorConfig {
 
 
 	/**
-	 *	Returns the buttons array of the wrapped config
-	 *	
-	 *	@return array
+	 * Returns the buttons array of the wrapped config
+	 *
+	 * @return array
 	 */
 	function getButtons() {
 		return $this->getConfig()->buttons;
@@ -110,6 +107,4 @@ class CustomHTMLEditorConfig extends HTMLEditorConfig {
 		return $this->getConfig()->plugins;
 	}
 
-
-	
 }
